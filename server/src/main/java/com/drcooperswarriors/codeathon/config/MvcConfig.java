edd121fa4dbaa32bb032238b0d6f338761bc6265
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-04-08 20:20:24 -0500

    Moved client app to be served from spring

---
+++ server/src/main/java/com/drcooperswarriors/codeathon/config/MvcConfig.java
@@ -0,0 +1,27 @@
+package com.drcooperswarriors.codeathon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(
+            ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/WEB-INF/view/react/build/static/");
+        registry.addResourceHandler("/*.js")
+                .addResourceLocations("/WEB-INF/view/react/build/");
+        registry.addResourceHandler("/*.json")
+                .addResourceLocations("/WEB-INF/view/react/build/");
+        registry.addResourceHandler("/*.ico")
+                .addResourceLocations("/WEB-INF/view/react/build/");
+        registry.addResourceHandler("/index.html")
+                .addResourceLocations("/WEB-INF/view/react/build/index.html");
+    }
+}
