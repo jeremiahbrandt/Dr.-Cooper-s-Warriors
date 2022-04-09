126d257f23ec889352eb40d7bd8609eafd4dbefd
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2022-04-09 10:24:26 -0500

    Added Category Input Box

--- server/src/main/webapp/WEB-INF/view/react/src/components/Events.js
+++ server/src/main/webapp/WEB-INF/view/react/src/components/Events.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import CatBox from './CatBox';
+
+
+
 
 function Events(){
     return (
     <div style={{ margin: "0 auto", height: '80vh', width: '80%' }}>
+        <CatBox></CatBox>
         <GoogleMapReact
           bootstrapURLKeys={{}}
           defaultCenter={{
