1f733e57147c24033ba9d040d3d6b57f9f04d623
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2022-04-09 09:41:03 -0500

    Google Map Added
    Added Google-Map-React

--- server/src/main/webapp/WEB-INF/view/react/src/components/Events.js
+++ server/src/main/webapp/WEB-INF/view/react/src/components/Events.js
@@ -1,7 +1,20 @@
-import GoogleMap from "../google-map/GoogleMap";
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 function Events(){
-<GoogleMap></GoogleMap>
+    return (
+    <div style={{ margin: "0 auto", height: '80vh', width: '80%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{}}
+          defaultCenter={{
+            lat: 39.0997,
+            lng: -94.578331
+          }}
+          defaultZoom={12}
+        >
+        </GoogleMapReact>
+      </div>
+    )
 }
 
 export default Events;
