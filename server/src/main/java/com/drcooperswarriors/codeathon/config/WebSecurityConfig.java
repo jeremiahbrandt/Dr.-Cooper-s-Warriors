bde36bd3f73a2bee7b7c977d5770ed1d395d75a6
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-04-09 19:29:03 -0500

    Moved client app

--- server/src/main/java/com/drcooperswarriors/codeathon/config/WebSecurityConfig.java
+++ server/src/main/java/com/drcooperswarriors/codeathon/config/WebSecurityConfig.java
@@ -62,76 +62,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        // Enable CORS and disable CSRF
-//        http = http.cors().and().csrf().disable();
-//
-//        // Set session management to stateless
-//        http = http
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and();
-//
-//        // Set unauthorized requests exception handler
-//        http = http
-//                .exceptionHandling()
-//                .authenticationEntryPoint(
-//                        (request, response, ex) -> {
-//                            response.sendError(
-//                                    HttpServletResponse.SC_UNAUTHORIZED,
-//                                    ex.getMessage()
-//                            );
-//                        }
-//                )
-//                .and();
-//
-//        // Set permissions on endpoints
-//        http.authorizeRequests()
-//                // Our public endpoints
-//                .antMatchers("/api/public/**").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/book/**").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/book/search").permitAll()
-//                // Our private endpoints
-//                .anyRequest().authenticated();
-//
-//        // Add JWT token filter
-//        http.addFilterBefore(
-//                jwtTokenFilter,
-//                UsernamePasswordAuthenticationFilter.class
-//        );
-
         http.csrf().disable().authorizeRequests()
-                //...
-                .antMatchers(
-                        HttpMethod.GET,
-                        "/index*", "/static/**", "/*.js", "/*.json", "/*.ico", "/login/oauth2/code/google")
+                .antMatchers(HttpMethod.GET,"/index*", "/static/**", "/*.js", "/*.json", "/*.ico", "/api/*")
                 .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login")
-                .loginProcessingUrl("/perform_login")
-                .defaultSuccessUrl("http://localhost:3000/",true)
-                .failureUrl("/login?error=true")
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(oauthUserService)
-                .and()
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
-                        userService.processOAuthPostLogin(oauthUser.getEmail());
-                        String jwt = jwtUtils.generateJwtToken(authentication);
-                        String token = (new JwtResponse(jwt, new Long(1), oauthUser.getEmail(), oauthUser.getName(), new ArrayList<>())).getAccessToken();
-                        System.out.println(token);
-                        response.setHeader("Authorization", token);
-                        response.sendRedirect("http://localhost:3000/");
-                    }
-                });
+                .anyRequest().authenticated();
     }
-
-    @Autowired
-    private CustomOAuth2UserService oauthUserService;
 }
--- server/src/main/java/com/drcooperswarriors/codeathon/controller/MainController.java
+++ server/src/main/java/com/drcooperswarriors/codeathon/controller/MainController.java
@@ -1,51 +1,21 @@
 package com.drcooperswarriors.codeathon.controller;
 
-import com.drcooperswarriors.codeathon.payload.response.JwtResponse;
-import com.drcooperswarriors.codeathon.security.CustomOAuth2User;
 import com.drcooperswarriors.codeathon.security.JwtUtils;
-import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
 
 @Controller
 @CrossOrigin
 public class MainController {
-    @RequestMapping(value = "/app")
-    public String index() {
-        return "index";
-    }
-
     @Autowired
     JwtUtils jwtUtils;
 
     @CrossOrigin
     @GetMapping("/api/test")
     public ResponseEntity getAllExamples() {
-
-//        return ResponseEntity.ok(
-//                "Hello World"
-//        );
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String jwt = jwtUtils.generateJwtToken(authentication);
-
-        CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
-
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                new Long(1),
-                oauthUser.getEmail(),
-                oauthUser.getName(),
-                new ArrayList<>()));
+        return ResponseEntity.ok("Hello World");
     }
-
-
 }
