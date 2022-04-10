6ce668ef29bf39927176cb3d8d7a05e0ac7ca5b0
Aaron7Thole <aaronr2017@gmail.com>
2022-04-09 23:03:09 -0500

    SQL

---
+++ SQL/Untitled.sql
@@ -0,0 +1,15 @@
+Insert into grops values(
+	(1, "Bird Watching", "The Whos","Pete Townshend"),
+    (2, "Speed Walking", "The Walking Deads" , "Bryan Cranston"),
+    (3, "Outdoor Chess", "Chess Gambits", "Magnus Carlsen"),
+    (4, "Knitting", "Knitting Club", "Ruth"),
+    (5, "Rock Climbing", "Rock Climbing", "Chad"),
+    (6, "Pottery Class", "The Ghost", "Patrick Swayze"),
+    (7, "Flower Planting", "Community Gardeners","Karin Ries"),
+    (8, "Dungeons and Dragons" "The Stormcloaks" "Ash J. Williams"),
+    (9, "Book Club" "Book Club", "LeVar Burton"),
+    (10,"Kite Flying", "The Kite Runners", "Khaled Hosseini"),
+    (11,"Chess Boxing", "MMA to D4", "Mike Tyson"),
+    (12,"Hiking", "Hike Tyson" "Jeff Probst"),
+    (13,"Bike Club", "Tour de Chance", "lance armstrong")
+);
