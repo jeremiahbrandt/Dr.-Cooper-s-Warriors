196019b9b7e3285dd58068748b899706db1c95b6
Aaron7Thole <aaronr2017@gmail.com>
2022-04-10 11:05:35 -0500

    Group popup

--- client/src/components/CreateGroupModal.js
+++ client/src/components/CreateGroupModal.js
@@ -14,21 +14,6 @@ export default function CreateGroupModal(props) {
                     <input type="text" />
                 </label>
             </Modal.Body>
-            <Modal.Body>
-                <label className="label-description">Enter Group Description:
-                    <input type="text" />
-                </label>
-            </Modal.Body>
-            <Modal.Body>
-                {/* <DropdownMenu userName="Chris Smith">
-                    <MenuItem text="Home" location="/home" />
-                    <MenuItem text="Edit Profile" location="/profile" />
-                    <MenuItem text="Change Password" location="/change-password" />
-                    <MenuItem text="Privacy Settings" location="/privacy-settings" />
-                    <MenuItem text="Delete Account" onClick={this.deleteAccount} />
-                    <MenuItem text="Logout" onClick={this.logout} />
-                </DropdownMenu> */}
-            </Modal.Body>
             <Modal.Footer className="d-flex justify-content-between">
                 <Button variant="danger" onClick={handleClose}>
                     Cancel Reservation
