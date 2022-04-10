78a1a056f3f1624744ef27d2abe14c2f0902f154
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2022-04-10 14:30:03 -0500

    Fixed the WORLD!#@%!@#%!@

--- server/src/main/java/com/drcooperswarriors/codeathon/controller/UserController.java
+++ server/src/main/java/com/drcooperswarriors/codeathon/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.drcooperswarriors.codeathon.controller;
 
-import com.drcooperswarriors.codeathon.model.Event;
-import com.drcooperswarriors.codeathon.model.Group;
-import com.drcooperswarriors.codeathon.model.UserResponse;
-import com.drcooperswarriors.codeathon.model.User;
-import com.drcooperswarriors.codeathon.repository.UserRepository;
+import com.drcooperswarriors.codeathon.model.*;
+import com.drcooperswarriors.codeathon.repository.*;
 import com.drcooperswarriors.codeathon.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +18,16 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    private EventParticipantsRepository eventParticipantsRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
     @GetMapping("/api/user")
     public ResponseEntity getAllExamples(@RequestParam(required = true) Integer id) {
         User user = userRepository.getUserById(id);
@@ -35,8 +42,26 @@ public class UserController {
 
     @PostMapping("/api/users")
     User newUser(@RequestBody User user) {
+
         return userRepository.save(user);
     }
+
+
+    @GetMapping("/api/getUserInformation")
+    public ResponseEntity getUserInfo(@RequestParam(required = true) Integer id){
+        User user = userRepository.getUserById(id);
+
+        if(user != null) {
+            List<Event> events = eventParticipantsRepository.getEventsByUsername(user);
+            List<Group> groups = eventParticipantsRepository.getGroupsByUsername(user);
+            UserResponse ur = new UserResponse(user, events, groups );
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
 
     // @GetMapping( "/api/users")
     // public List<User> getUsers(){
--- server/src/main/java/com/drcooperswarriors/codeathon/repository/GroupRepository.java
+++ server/src/main/java/com/drcooperswarriors/codeathon/repository/GroupRepository.java
@@ -4,4 +4,5 @@ import com.drcooperswarriors.codeathon.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GroupRepository extends JpaRepository<Group, Integer> {
+
 }
