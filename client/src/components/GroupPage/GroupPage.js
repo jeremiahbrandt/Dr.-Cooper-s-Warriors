ffc940ba5e00d689f9d7be326e5603aff8b91e13
Aaron7Thole <aaronr2017@gmail.com>
2022-04-10 13:07:35 -0500

    Create popup

--- client/src/components/GroupPage/GroupPage.js
+++ client/src/components/GroupPage/GroupPage.js
@@ -1,42 +1,108 @@
 import React from "react"
+import { Col, Row, Card, Modal, ListGroupItem, ListGroup } from "react-bootstrap";
 import Button from 'react-bootstrap/Button'
 import Posts from './Posts'
 import styled from "styled-components"
-import { useEffect , useState} from "react"
+import axios from "axios";
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import CreateEventModal from "../CreateEventModal"
-
-export default function GroupPage(props){
+import "./CreateEvent.css";
+export default function GroupPage(props) {
     const { id } = useParams();
+    useEffect(() => {
+    }, [])
+
     const [show, setShow] = useState(false);
-    useEffect(()=>{
-    },[])
-    return(
-        
-    <Container>
-        <Flex>
-        <GroupName as="h1">Morning Yoga</GroupName>
-        <Image>
-            <img src="rex.jpg"/>
-        </Image>
-        </Flex>
-        {/* <Bio as="p">{}</Bio> */}
-        <Bio as="p">In the yogic discipline, the morning is considered as a “divine time” and when an individuals spiritual energy is at its peak. 
-        This is a group that meets at 6 in the morning to do yoga and connect with ourselfs. Everyone is welcome to join.
-        </Bio>
-        <Push>
-            <Button variant="primary" type="submit">Create Event</Button>
-        </Push>
+    const [showGroup, setShowGroup] = useState(false);
+    const [modalEvent, setModalEvent] = useState();
 
-        <PostBody>
-            <Posts posts={tempPosts} />
-        </PostBody>
-    </Container>
-        
+    const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
+    function handleShowCreateGroupModal() {
+        setShowCreateGroupModal(true)
+    }
+    function handleHideCreateGroupModal() {
+        setShowCreateGroupModal(false)
+    }
+    function handleClose() {
+        setShow(false)
+    }
+    //Event Popup
+    function handleShow(event) {
+        setShow(true);
+    }
+    //Group Popup
+    function handleGroupClick(event) {
+        setShowGroup(true);
+    }
+    function handleGroupClose() {
+        setShowGroup(false)
+    }
+    return (
+
+        <Container>
+            <Flex>
+                <GroupName as="h1">Morning Yoga</GroupName>
+                <Image>
+                    <img src="rex.jpg" />
+                </Image>
+            </Flex>
+            {/* <Bio as="p">{}</Bio> */}
+            <Bio as="p">In the yogic discipline, the morning is considered as a “divine time” and when an individuals spiritual energy is at its peak.
+                This is a group that meets at 6 in the morning to do yoga and connect with ourselfs. Everyone is welcome to join.
+            </Bio>
+            <Push>
+                <Button variant="primary" type="submit" onClick={handleShow}>Create Event</Button>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            Create Event
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <label className="event-name-label"><b>Group Name:</b>
+                            <input className="group-name-input" type="text" />
+                        </label>
+                        <div className="event-address">
+                            <label className="event-address-label"><b>Event Address:</b></label>
+                            <textarea className="event-address-text"
+                                rows="2"
+                            />
+                        </div>
+                        <label className="event-date-label"><b>Group Date:</b>
+                            <input className="group-date-input" type="Date" />
+                        </label>
+                        <label className="event-time-label"><b>Group Time:</b>
+                            <input className="group-time-input" type="time" />
+                        </label>
+                        <div className="event-description">
+                            <label className="event-description-label"><b>Group Description:</b></label>
+                            <textarea className="event-description-text"
+                                rows="2"
+                            />
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer className="d-flex justify-content-between">
+                        <Button variant="success" onClick={handleClose}>
+                            Cancel Reservation
+                        </Button>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </Push>
+
+            <PostBody>
+
+                <Posts posts={tempPosts} />
+            </PostBody>
+        </Container>
 
 
-        
-    
+
+
+
     )
 }
 
