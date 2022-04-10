d07a588a4ae8d32fbe75f0438b6aad74fa1bb30a
Jeremiah Brandt <60268813+jeremiahbrandt@users.noreply.github.com>
2022-04-10 01:10:42 -0500

    Update event.sql

--- SQL/event.sql
+++ SQL/event.sql
@@ -1,14 +1,118 @@
-Insert into events values
-(1, "Blue Valley Park Rd E", NULL, 39.081415, -94.509650, "Who Who's Watching"),
-(2, "4209 Oak St", NULL, 39.050584, -94581790, "March of the Walkers"),
-(3, "Broadway Boulevard", NULL, 39.094037, -94.588452, "Ton of Chess"),
-(4, "North 13th Street", NULL, 39.116008, -94.643369, "Ruth's Knitting Night"),
-(5, "North Richards Road", NULL, 39.117640, -94.590790, "Rock Climbing Extreme"),
-(6, "East 8th Street", NULL, 39.103762, -94.576278, "The Swayze Experience"),
-(7, "OK Street", NULL, 39.084426, -94.585519, "Communities Planting Event"),
-(8, "West 39th Avenue", NULL, 39.057255, -94.610676, "Dungeon's in the KU"),
-(9,"3050 Prospect Ave, Kansas City", Null, "39.08283","-94.54712","Lucile H. Bluford Branch"),
-(10,"15120 W 63rd St, Shawnee", Null, "39.01568", "-94.76276" "Shawnee"),
-(11,"2000 Prospect Ave, Kansas City", Null,"39.08854","-94.55362","Blue Park"),
-(12,"18695 S Lackman Rd, Olathe",Null,38.79151,-94.75569,"Wolf Creek"),
-(13,"8045 Santa Fe Dr, Overland Park",NULL,"38.98269","-94.67201", "Tom Park");
+INSERT INTO events(event_id, address, date, lat, log, name, group_id)
+VALUES (
+        1,
+        "Blue Valley Park Rd E",
+        NULL,
+        39.081415,
+        -94.509650,
+        "Who Who's Watching",
+        1
+    ),
+    (
+        2,
+        "4209 Oak St",
+        NULL,
+        39.050584,
+        -94581790,
+        "March of the Walkers",
+        2
+    ),
+    (
+        3,
+        "Broadway Boulevard",
+        NULL,
+        39.094037,
+        -94.588452,
+        "Ton of Chess",
+        3
+    ),
+    (
+        4,
+        "North 13th Street",
+        NULL,
+        39.116008,
+        -94.643369,
+        "Ruth's Knitting Night",
+        4
+    ),
+    (
+        5,
+        "North Richards Road",
+        NULL,
+        39.117640,
+        -94.590790,
+        "Rock Climbing Extreme",
+        5
+    ),
+    (
+        6,
+        "East 8th Street",
+        NULL,
+        39.103762,
+        -94.576278,
+        "The Swayze Experience",
+        6
+    ),
+    (
+        7,
+        "OK Street",
+        NULL,
+        39.084426,
+        -94.585519,
+        "Communities Planting Event",
+        7
+    ),
+    (
+        8,
+        "West 39th Avenue",
+        NULL,
+        39.057255,
+        -94.610676,
+        "Dungeon's in the KU",
+        8
+    ),
+    (
+        9,
+        "3050 Prospect Ave, Kansas City",
+        Null,
+        "39.08283",
+        "-94.54712",
+        "Lucile H. Bluford Branch",
+        9
+    ),
+    (
+        10,
+        "15120 W 63rd St, Shawnee",
+        Null,
+        "39.01568",
+        "-94.76276",
+        "Shawnee",
+        10
+    ),
+    (
+        11,
+        "2000 Prospect Ave, Kansas City",
+        Null,
+        "39.08854",
+        "-94.55362",
+        "Blue Park",
+        11
+    ),
+    (
+        12,
+        "18695 S Lackman Rd, Olathe",
+        Null,
+        38.79151,
+        -94.75569,
+        "Wolf Creek",
+        12
+    ),
+    (
+        13,
+        "8045 Santa Fe Dr, Overland Park",
+        NULL,
+        "38.98269",
+        "-94.67201",
+        "Tom Park",
+        13
+    );
