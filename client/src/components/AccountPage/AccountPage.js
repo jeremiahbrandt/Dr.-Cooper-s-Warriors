062c26e1287167ef1456a804a2360174c273b87a
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2022-04-10 21:44:31 -0500

    added dummy data

--- client/src/components/AccountPage/AccountPage.js
+++ client/src/components/AccountPage/AccountPage.js
@@ -13,10 +13,10 @@ export default function AccountPage(props) {
     const [userInfo, setUserInfo] = useState();
 
     const getAllInformation = async () => {
-        if(token != null){
+
         const id = searchParams.get("id")
         console.log(id);
-        }
+        
     }
 
     
@@ -190,35 +190,16 @@ const temporaryGroups = [
 ]
 
 const temporaryEvents = [
-    'Event 1',
-    'Event 2',
-    'Event 3',
-    'Event 4',
-    'Event 5',
-    'Event 6',
-    'Event 7',
-    'Event 8',
-    'Event 9',
-    'Event 10',
-    'Event 11',
-    'Event 12',
-    'Event 13',
-    'Event 14',
-    'Event 15',
-    'Event 16',
-    'Event 17',
-    'Event 18',
-    'Event 19',
-    'Event 20',
-    'Event 21',
-    'Event 22',
-    'Event 23',
-    'Event 24',
-    'Event 25',
-    'Event 26',
-    'Event 27',
-    'Event 28',
-    'Event 29',
+    'DND Night w/ Nathan',
+    'Zoomba Granny Night',
+    'Fruit Snack Connoissuer Night',
+    'Coffee with the Boys',
+    'Algorithm Club w/ Prof. Cooper',
+    'Emboridery Club #22',
+    'Aarons Corn Convention',
+    'New iPhone midnight release',
+    'Hydration Nation',
+    'KC Hike Club',
 ]
 
 const temporaryUser = {
